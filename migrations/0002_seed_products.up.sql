223905e415390001766952cd6fb512e012be3fad
yong quan <yongquan02@gmail.com>
2025-04-17 05:39:09 +0800

    feat: add migration file to populate db

---
+++ migrations/0002_seed_products.up.sql
@@ -0,0 +1,8 @@
+INSERT INTO products (id, name, type, price, description, picture_url) VALUES
+                                                                           (4, 'Leather Sofa', 'Furnitures', 505.00, 'Italian Genuine Leather Sofa', '/images/1744832003978371797.jpg'),
+                                                                           (5, 'Leather Carpet', 'Furnitures', 505.00, 'Italian Genuine Leather Sofa', '/images/1744832053922520055.jpg'),
+                                                                           (6, 'Samsung A57', 'Electronics', 700.00, 'Black\n256GB\nSnapdragon Gen 1', '/images/1744837250428555967.jpg'),
+                                                                           (7, 'Harry Potter Books', 'Books', 200.00, 'Entire collection of Harry potter', '/images/1744834544366694064.jpg'),
+                                                                           (8, 'dog sculpture', 'Furnitures', 80.00, 'Wooden dog sculpture', '/images/1744834599523196897.jpg'),
+                                                                           (9, 'Kitchen set', 'Furnitures', 5000.00, 'Oven, Dish washer and Fridge', '/images/1744834664611125630.jpg'),
+                                                                           (10, 'Kid''s Toys', 'Toys', 20.00, 'Teddy Bear Plush\nPlastic Lego\nBeads\nRubber Duck\nLion', '/images/1744838198189565810.jpg');
